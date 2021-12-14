27f4ea31550ad337f088f224820e51c62f5e2f5d
pjovan <jovan.popovic@outlook.com>
2021-12-14 21:39:20 +0100

    Hopefully it works

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -49,10 +48,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
 //		return source;
 //	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().authorizeRequests().anyRequest().permitAll().and().csrf().disable();
-	}
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.cors().and().authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+//	}
 
 //	@Override
 //	protected void configure(HttpSecurity http) throws Exception {
