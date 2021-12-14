ff3c2a63f8953e3ac507fc1672b4d96d4e7ec6ba
pjovan <jovan.popovic@outlook.com>
2021-12-14 21:35:18 +0100

    no message

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,30 +1,20 @@
 package com.example.demo.config;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.example.demo.filter.JwtFilter;
 import com.example.demo.util.JwtUtil;
 
-@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 	@Autowired
@@ -52,35 +42,47 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
 		return super.authenticationManagerBean();
 	}
 
+//	@Bean
+//	protected CorsConfigurationSource corsConfigurationSource() {
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+//		return source;
+//	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// Enable CORS and disable CSRF
-		http = http.csrf().disable();
-
-		// Set session management to stateless
-		http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and();
-
-		// Set unauthorized requests exception handler
-		http = http.exceptionHandling().authenticationEntryPoint((request, response, ex) -> {
-			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
-		}).and();
-
-		// Set permissions on endpoints
-		http.authorizeRequests()
-				// Our public endpoints
-				.antMatchers("/authenticate", "/forgot-password", "/reset-password").permitAll().antMatchers()
-				.permitAll()
-
-				// Our private endpoints
-				.anyRequest().authenticated().and().formLogin().permitAll().loginProcessingUrl("/login")
-				.successHandler((req, res, auth) -> res.setStatus(HttpStatus.NO_CONTENT.value()))
-				.failureHandler(new SimpleUrlAuthenticationFailureHandler()).and().exceptionHandling()
-				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)).and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-		// Add JWT token filter
-		http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+		http.cors().and().authorizeRequests().anyRequest().permitAll().and().csrf().disable();
 	}
+
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		// Enable CORS and disable CSRF
+//		http = http.csrf().disable();
+//
+//		// Set session management to stateless
+//		http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and();
+//
+//		// Set unauthorized requests exception handler
+//		http = http.exceptionHandling().authenticationEntryPoint((request, response, ex) -> {
+//			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
+//		}).and();
+//
+//		// Set permissions on endpoints
+//		http.authorizeRequests()
+//				// Our public endpoints
+//				.antMatchers("/authenticate", "/forgot-password", "/reset-password").permitAll().antMatchers()
+//				.permitAll()
+//
+//				// Our private endpoints
+//				.anyRequest().authenticated().and().formLogin().permitAll().loginProcessingUrl("/login")
+//				.successHandler((req, res, auth) -> res.setStatus(HttpStatus.NO_CONTENT.value()))
+//				.failureHandler(new SimpleUrlAuthenticationFailureHandler()).and().exceptionHandling()
+//				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)).and().sessionManagement()
+//				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//
+//		// Add JWT token filter
+//		http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+//	}
 
 //	// Used by spring security if CORS is enabled.
 //	@Bean
@@ -96,10 +98,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
 //		return source;
 //	}
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/api/**").allowedOrigins("http://localhost:3000")
-				.allowedMethods("PUT", "DELETE", "POST", "GET").allowedHeaders("Authorization");
-
-	}
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/api/**").allowedOrigins("http://localhost:3000")
+//				.allowedMethods("PUT", "DELETE", "POST", "GET").allowedHeaders("Authorization");
+//
+//	}
 }
